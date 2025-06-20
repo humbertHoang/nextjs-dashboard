74ffccb94073d859caf7baa5444620a5da489112
CPHAT001 <csphat1@gmail.com>
2025-06-20 20:13:59 +0700

    update

---
+++ auth.config.ts
@@ -0,0 +1,24 @@
+﻿import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+export const authConfig = {
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user
+            const isOnDashBoard = nextUrl.pathname.startsWith('/dashboard')
+            if (isOnDashBoard) {
+                if (isLoggedIn) return true
+                return false
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl))
+            }
+            return true
+        }
+    },
+    providers: [Credentials({
+
+    })]
+} satisfies NextAuthConfig;
